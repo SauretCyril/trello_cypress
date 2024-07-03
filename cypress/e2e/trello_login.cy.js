a58babc14ed1b40f4c1afb6926d4f027c34bc8ec
SAURET Cyril <sauretcyril@outlook.fr>
2024-07-03 11:16:58 +0200

    Co-authored-by: KHAMED Smaïl <khameds@gmail.com> Co-authored-by: Lilicoco42 <Lilicoco42@users.noreply.github.com>

--- cypress/e2e/trello_login.cy.js
+++ cypress/e2e/trello_login.cy.js
@@ -1,14 +1,18 @@
 describe("template spec", () => {
   before(() => {
-    cy.fixture("users.json").as("userData");
+    cy.fixture("users.json").as("user");
   });
 
   beforeEach(() => {
     cy.visit("https://trello.com/u/dudukain/boards");
   });
+
   //cy.wrap(cy.get("@data")).user
   it("login dudu", () => {
-    cy.get("[data-testid='ui-email-signup-input']").last().type();
+    cy.get("@user").then((user) => {
+      cy.get("[data-testid='ui-email-signup-input']").last().type(user.email);
+    });
+
     cy.get('button:contains("Sign up")').last().click();
   });
 });
